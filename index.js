7ff41cd5ed067849a7c0a98c0221316795429157
SyuusukeFuji <143052059+SyuusukeFuji@users.noreply.github.com>
2023-12-05 22:31:15 -0400

    Adding the files

---
+++ index.js
@@ -0,0 +1,14 @@
+let accordion=  document.getElementsByClassName("question");
+
+for(let i=0; i<accordion.length; i++){
+    accordion[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+
+        let panel= this.nextElementSibling;
+        if(panel.style.display==="block"){
+            panel.style.display="none";
+        }else{
+            panel.style.display="block";
+        }
+    });
+}
